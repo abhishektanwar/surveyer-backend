0bb40057980ab854e879acf3e9fb9dbcefc80ef0
Abhishek Tanwar <mithyalabs1@Abhisheks-Macbook-Air.local>
2022-01-11 07:35:02 +0530

    initial app

---
+++ index.js
@@ -0,0 +1,17 @@
+//here in line 4 we are using commonjs modules
+// commonjs is a system implemented in nodejs for requiring and sharing
+// code b/w different files
+const express = require('express');
+
+// import express from 'express' -> this uses ES2015/ES6 modules, nodejs does not have
+// support for ES2015 modules
+// there could be multiple apps aswell 
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' })
+});
+
+const PORT = process.env.PORT || 5000;
+// express listening for requests on post 5000
+app.listen(PORT);
